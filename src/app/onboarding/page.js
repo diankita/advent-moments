ce374476140c66bb9851d966226565a9ff8cbea4
Diana <sulkova.diana@gmail.com>
2024-01-16 21:02:37 +0100

    create  and style onboarding screens

--- src/app/onboarding/page.js
+++ src/app/onboarding/page.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import OnboardingStepper from '@/app/components/onboarding-stepper';
 
 export default function Page() {
-  const currentPage = 1;
+  const currentPage = 2;
 
   return (
     <main className="flex min-h-screen flex-col justify-between px-6 py-14 bg-emerald-900">
@@ -14,22 +14,26 @@ export default function Page() {
         width={500}
         height={620}
       />
-      <h1 className="text-xl text-center text-white">
-        <p className="mb-2 font-semibold">
-          Online advent calendars are awesome!
+      <h1 className="text-lg text-center text-white">
+        <p className="mb-2 font-semibold text-xl">
+          Personalize your calendar 👩‍🎨
         </p>
-        And more sustainable 🌱
+        Choose a title, add your messages & pictures and share with your loved
+        one
       </h1>
       <div className="flex justify-between items-center">
-        <button className="opacity-0 rounded-lg bg-none py-1 px-2 text-lg text-white transition-colors hover:outline-green-400    focus-visible:outline-green-600">
-          Next
-        </button>
-        <OnboardingStepper currentPage={currentPage} />
         <Link
-          href="/onboarding"
+          href="/"
           className="rounded-lg bg-none py-1 px-2 text-lg text-white transition-colors hover:outline-green-400 focus-visible:outline-green-600"
         >
-          Next
+          Back
+        </Link>
+        <OnboardingStepper currentPage={currentPage} />
+        <Link
+          href="/edit"
+          className="rounded-lg bg-none py-1 px-2 text-lg text-white transition-colors hover:outline-green-400 focus-visible:outline-green-600"
+        >
+          Start
         </Link>
       </div>
     </main>
